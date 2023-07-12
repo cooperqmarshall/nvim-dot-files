94452d3be97aea333e7bbc79476227e8cf789390
cooper <cooperqmarshall@gmail.com>
2023-07-11 17:30:33 -0700

    telescope

--- lua/packer_setup.lua
+++ lua/packer_setup.lua
@@ -56,8 +56,14 @@ return require('packer').startup(function(use)
   use 'feline-nvim/feline.nvim'
 
   -- workflow
-  use { 'ibhagwan/fzf-lua',
-    requires = { 'kyazdani42/nvim-web-devicons' }
+--  use { 'ibhagwan/fzf-lua',
+--    requires = { 'kyazdani42/nvim-web-devicons' }
+--  }
+  use 'kyazdani42/nvim-web-devicons'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+  -- or                            , branch = '0.1.x',
+    requires = { 'nvim-lua/plenary.nvim' }
   }
   use 'tpope/vim-fugitive'
   use { 'ThePrimeagen/harpoon',
