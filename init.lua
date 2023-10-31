f8360ff5c4b1b4583cd9b319f457d60e51d2cf36
Cooper Marshall <cooper.marshall@imagen.ai>
2023-10-31 08:05:20 -0700

    Fixed spelling in error message

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Cooper's Neovim Config
-if not pcall(require,'options') then print("options was not loaded") end
-if not pcall(require,'keymaps') then print("keymaps was not loaded") end
+if not pcall(require,'options') then print("options were not loaded") end
+if not pcall(require,'keymaps') then print("keymaps were not loaded") end
 if not pcall(require,'packer_setup') then print("packer was not loaded") end
 
